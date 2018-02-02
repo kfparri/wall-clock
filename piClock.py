d1b4b8cd67db4467addfc4c75394b491101163d8
Kyle Parrish <kfparri@gmail.com>
2018-02-02 15:34:57 -0600

    Adding resources and some code changes

--- piClock.py
+++ piClock.py
@@ -12,23 +12,11 @@
 # imports
 import datetime, pygame, sys, datetime
 from pygame.locals import *
-from Tkinter import *
 
 # global variables
 red_hour = 6
 red_minute = 0
-
-# I'm having some issues re-opening the root window after it has been
-#   closed.  This is because TK does not want you to run the mainloop
-#   more than once.  I have found an interesting web page here:
-#   http://grapevine.com.au/~wisteria/tkfront/pygame_tkinter.html
-#   that shows a different way to show the window.  I need to check
-#   this out.
-root = Tk()
-root.geometry('200x200')
-    
-var_hour = StringVar()
-var_minute = StringVar()
+show_settings = False
 
 # constants
 SCREEN_WIDTH = 640
@@ -72,37 +60,82 @@ def decrease_minute():
         red_minute = 59
     var_minute.set(red_minute)
 
-def define_window():
-    var_hour.set(red_hour)
-    var_minute.set(red_minute)
+def displayClock(display_font, screen, background, settings, blink):    
+    time = datetime.datetime.now()
+    newtime = datetime.datetime.now()
+        
+    if time.minute != newtime.minute:
+        if newtime.hour > 12:
+            hour = newtime.hour - 12
+        else:
+            hour = newtime.hour
 
-    hour_up = Button(root, text="up", command=increase_hour)
-    hour_down = Button(root, text="down", command=decrease_hour)
-    minute_up = Button(root, text="up", command=increase_minute)
-    minute_down = Button(root, text="down", command=decrease_minute)
+        minute = newtime.minute
+    else:
+        if time.hour > 12:
+            hour = time.hour - 12
+        else:
+            hour = time.hour
+        minute = time.minute
+        
+    displayTime = str(hour).zfill(2) + ":" + str(minute).zfill(2)
 
-    hour_lbl = Label(root, textvariable=var_hour)
-    minute_lbl = Label(root, textvariable=var_minute)
-    lblhour = Label(root, text="Hour")
-    lblmin = Label(root, text="Minute")
+    color = MATRIX_GREEN
+    
+    if hour < red_hour:
+        color = RED
+    elif hour == red_hour and minute < red_minute:
+        color = RED
+    
+    text = display_font.render(str(displayTime), 1, color)
+    colonText = display_font.render(":", 1, MATRIX_GREEN)
 
-    hour_up.grid(row=0, column=1)
-    minute_up.grid(row=0, column=3)
+    screen.blit(background, (0,0))
+    
+    if blink:
+        pygame.draw.rect(text, BLACK,
+                        (text.get_width() / 2 - colonText.get_width() / 2,
+                        text.get_height() /2 - colonText.get_height() / 2,
+                        colonText.get_width(), colonText.get_height()))
 
-    lblhour.grid(row=1, column=0)
-    hour_lbl.grid(row=1, column=1)
-    lblmin.grid(row=1, column=2)
-    minute_lbl.grid(row=1, column=3)
+    screen.blit(text, ((SCREEN_WIDTH / 2) - text.get_width() / 2,
+                        (SCREEN_HEIGHT / 2) - text.get_height() / 2))
+    
+    imgRect = settings.get_rect()
+    imgRect.x = 0
+    imgRect.y = 0
 
+    screen.blit(settings, imgRect)
 
-    hour_down.grid(row=2, column=1)
-    minute_down.grid(row=2, column=3)
+    pygame.display.flip()
+
+def display_settings(background, screen, display_font):
+    color = MATRIX_GREEN
+
+    up = pygame.image.load("up.png")
+    down = pygame.image.load("down.png")
+
+    hour_text = display_font.render(str(red_hour), 1, color)
+    hour_loc = (100, 100)
+
+    minute_text = display_font.render(str(red_minute), 1, color)
+    minute_loc = (400, 100)
+
+    screen.blit(background, (0,0))
+
+    screen.blit(hour_text, hour_loc)
+    screen.blit(minute_text, minute_loc)
+
+    pygame.display.flip()
+
 
 def main():
     pygame.init()
 
     # text
     TEXT_FONT = pygame.font.Font('freesansbold.ttf', 200)
+
+    IMG = pygame.image.load("settings.png")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Clock')
@@ -113,19 +146,11 @@ def main():
     screen.blit(background, (0,0))
     pygame.display.update()
     
-    time = datetime.datetime.now()
-    updateTime = True
+    #time = datetime.datetime.now()
     firstRun = True
-    blink = False
+    blink = False    
+    show_settings = False
 
-    # setup the tk stuff for the update window
-    define_window()    
-
-    #var_hour = StringVar()
-    #var_minute = StringVar()    
-    
-    #redHour = 3
-    #redMinute = 45
     last_ticks = pygame.time.get_ticks()
     
     while 1:
@@ -145,49 +170,18 @@ def main():
                     print "after main loop"
                     print red_hour
                     print red_minute
-                    #root = Tk()
-                    #define_window()
-                
-        newtime = datetime.datetime.now()
-        
-        if time.minute != newtime.minute:
-            if newtime.hour > 12:
-                hour = newtime.hour - 12
-            else:
-                hour = newtime.hour
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if IMG.get_rect().collidepoint(x,y):
+                    print "button Clicked"
+                    show_settings = True
 
-            minute = newtime.minute
+        # if I click the button, the screen will stop updating
+        if not show_settings:        
+            displayClock(TEXT_FONT, screen, background, IMG, blink)
         else:
-            if time.hour > 12:
-                hour = time.hour - 12
-            else:
-                hour = time.hour
-            minute = time.minute
-            
-        displayTime = str(hour).zfill(2) + ":" + str(minute).zfill(2)
+            display_settings(background, screen, TEXT_FONT)
 
-        color = MATRIX_GREEN
-        
-        if hour < red_hour:
-            color = RED
-        elif hour == red_hour and minute < red_minute:
-            color = RED
-        
-        text = TEXT_FONT.render(str(displayTime), 1, color)
-        colonText = TEXT_FONT.render(":", 1, MATRIX_GREEN)
-
-        screen.blit(background, (0,0))
-        
-        if blink:
-            pygame.draw.rect(text, BLACK,
-                         (text.get_width() / 2 - colonText.get_width() / 2,
-                          text.get_height() /2 - colonText.get_height() / 2,
-                          colonText.get_width(), colonText.get_height()))
-
-        screen.blit(text, ((SCREEN_WIDTH / 2) - text.get_width() / 2,
-                           (SCREEN_HEIGHT / 2) - text.get_height() / 2))
-        pygame.display.flip()
-        
         if firstRun:
             firstRun = False
 
