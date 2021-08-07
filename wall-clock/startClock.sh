16543779972c50d40909f7dd4e3313a0ee3d9a52
Kyle Parrish <kfparri@gmail.com>
2021-08-06 20:30:30 -0500

    Updated the start clock script so I can run it during my stream and not run the extra stuff in the file

--- wall-clock/startClock.sh
+++ wall-clock/startClock.sh
@@ -1,17 +1,17 @@
-sudo service ntp stop
-sudo ntpdate -s -u -b pool.ntp.org
-sudo service ntp start
+#sudo service ntp stop
+#sudo ntpdate -s -u -b pool.ntp.org
+#sudo service ntp start
 
-cd /home/pi/source/wall-clock
+#cd /home/pi/source/wall-clock
 
 git pull
 
-cd wall-clock
+#cd wall-clock
 
 # update the splash screen 
 # information for this was found here: https://www.raspberrypi.org/forums/viewtopic.php?t=197472
-sudo mv /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix/splash.png.bak
-sudo cp splash.png /usr/share/plymouth/themes/pix/splash.png
+#sudo mv /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix/splash.png.bak
+#sudo cp splash.png /usr/share/plymouth/themes/pix/splash.png
 
 // make sure you install requests
 //  pip install requests
